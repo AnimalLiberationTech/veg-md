5f4e7f737c7831ef669e1337ab4bb784135b3f1e
Anton Matusenco <anton@matusenco.com>
2025-09-26 21:18:07 +0300

    fix localhost images optimization

--- next.config.js
+++ next.config.js
@@ -6,6 +6,7 @@ const withNextIntl = createNextIntlPlugin();
 const nextConfig = {
   output: 'export',
   images: {
+    unoptimized: true,
     remotePatterns: [
       {
         protocol: "https",
