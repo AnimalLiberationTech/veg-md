7573d4adeb3867038e090badcb8b90351e4e4715
Anton Matusenco <anton@matusenco.com>
2026-05-16 01:35:48 +0300

    fetch gSheet from proxy service

--- src/constants.ts
+++ src/constants.ts
@@ -25,5 +25,3 @@ export const uvmSite = "https://uvem.org";
 export const bugReportUrl = "https://smtp-notifier.fra.appwrite.run/send-bug-report";
 export const gCalUrl = "https://veg-md.fra.appwrite.run/google/get-cal";
 export const gSheetUrl = "https://veg-md.fra.appwrite.run/google/get-sheet";
-export const expensesCsvUrl =
-  "https://docs.google.com/spreadsheets/d/e/2PACX-1vSu_lowuCcBP7SHm1Qn6UavMR96-HGtcbBBbrkS6us_G9o8WIreXjiednNh0oWDBcodLUNK2wVb3blo/pub?gid=610595844&single=true&output=csv";
